3f0b6f6a5bd5c2f2c883da5021173da038ea2413
DanilVerkhoturov <danil.verhoturov1@gmail.com>
2020-06-16 15:52:24 +0500

    1

--- Admin_Panel_Hotel/EditApplications/ShowApplicationVer2.cs
+++ Admin_Panel_Hotel/EditApplications/ShowApplicationVer2.cs
@@ -18,6 +18,7 @@ namespace Admin_Panel_Hotel
         {
             InitializeComponent();
             this.GridTable.Rows.Add("1", "Иванов Иван Иванович", "ННГ/Цех 136/112", "06.06.2020", "06.06.2020");
+            //Functions.NewlineProcessing(EmailsDataGridView, new string[] { "1", "Введите имя", "Введите электронную почту" });
             this.GridTable.Rows.Add("2", "Петров Петр Петрович", "ННГ/Цех 136/113", "06.06.2020", "06.06.2020");
         }
 
