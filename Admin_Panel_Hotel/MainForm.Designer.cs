153447aa839c1d79bcaa8dfc0e6cc4b8c34fc53e
Andrew89RUS <42917657+Andrew89RUS@users.noreply.github.com>
2020-07-27 22:09:48 +0500

    1

--- Admin_Panel_Hotel/MainForm.Designer.cs
+++ Admin_Panel_Hotel/MainForm.Designer.cs
@@ -716,7 +716,7 @@
             this.Controls.Add(this.ContentPanel);
             this.Controls.Add(this.TopPanel);
             this.Controls.Add(this.LeftMenu);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1920, 1024);
